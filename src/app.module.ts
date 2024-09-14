4e79d3fb408fc041160808be756b76b749807f59
Jorge M. Carrillo <jm.carrillo@uniandes.edu.co>
2024-09-14 14:42:19 -0500

    se crean entidades aerolinea, aeropuerto y persistencia

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AerolineaModule } from './aerolinea/aerolinea.module';
+import { AeropuertoModule } from './aeropuerto/aeropuerto.module';
+import { AeropuertoEntity } from './aeropuerto/aeropuerto.entity/aeropuerto.entity';
+import { AerolineaEntity } from './aerolinea/aerolinea.entity/aerolinea.entity';
+
+@Module({
+  imports: [AerolineaModule, AeropuertoModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'vuelos',
+      entities: [AerolineaEntity, AeropuertoEntity],
+      dropSchema: true,
+      synchronize: true,
+      keepConnectionAlive: true
+    })
+    ],  
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
